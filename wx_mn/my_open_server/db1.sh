9b3a1bf8ae1c2487a4642ef76e8b9bedf0c2e313
Papaya <307798536@qq.com>
2018-05-16 14:04:08 +0800

    Create db1.sh

---
+++ wx_mn/my_open_server/db1.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+cat > /opt/${1}_${2}_${3}_${4}.sh <<end
+#!/bin/bash
+### used for guofu db ###
+
+useradd a_mix_$2
+
+cd /home && cp -r a_mix_$1/* a_mix_$2
+
+sed -i 's/game_mix_$1/game_mix_$2/g' /home/a_mix_$2/backup/common_config.lua
+
+sed -i '/.*port.*= ${3}.*/s/= $3/= $4/g' /home/a_mix_$2/backup/common_config.lua
+
+sed -i '/current_area_id.*= ${1}.*/s/= $1/= $2/g' /home/a_mix_$2/backup/common_config.lua
+
+if [[ $1 -lt 10 && $2 -ge 10 ]];then
+	new1=$(echo $1| sed "s/0//")
+	sed -i '/current_area_id.*= ${new1}.*/s/= ${new1}/= $2/g' /home/a_mix_$2/backup/common_config.lua
+elif [[ $1 -lt 10 && $2 -lt 10 ]];then
+	new1=$(echo $1| sed "s/0//")
+  new2=$(echo $2| sed "s/0//")
+  sed -i '/current_area_id.*= ${new1}.*/s/= ${new1}/= ${new2}/g' /home/a_mix_$2/backup/common_config.lua
+fi
+
+sed -i 's/${3}9999/${4}9999/' /home/a_mix_$2/backup/config.*
+
+grep ${4}9 /home/a_mix_$2/backup/ -r
+
+#############################################################################
+if [ $3 -eq 3 ];then
+	sed -i '/.*port.*= ${4}306.*/s/= $4/= 3/g' /home/a_mix_$2/backup/common_config.lua
+fi
+#############################################################################
+
+cd /home/a_mix_$2/bin && rm -fr ./*
+
+cd /home/a_mix_$2/packet
+ls /home/a_mix_$2/packet|grep -v "HuiHe_Game" |xargs rm -fr
+
+#############################################################################
+sed -i 's/game_mix_$1/game_mix_$2/' /home/a_mix_$2/tools/backup_db_to_web.py
+
+sed -i 's/a_mix_$1/a_mix_$2/' /home/a_mix_$2/tools/up_file_to_ftp.py
+
+sed -i 's/game_mix_$1/game_mix_$2/' /home/a_mix_$2/tools/dump_role_to_web.py
+
+grep $2 /home/a_mix_$2/tools/ -r
+#############################################################################
+
+chown -R a_mix_$2:a_mix_$2 /home/a_mix_$2
+
+su - a_mix_$2 -c 'cd /home/a_mix_$2;bash update.sh'
+
+su - a_mix_$2 -c 'cd /home/a_mix_$2;bash do.sh start SCS'
+su - a_mix_$2 -c 'cd /home/a_mix_$2;bash do.sh start DBU'
+
+sleep 10
+
+su - a_mix_$2 -c 'cd /home/a_mix_$2;bash do.sh kill SCS'
+su - a_mix_$2 -c 'cd /home/a_mix_$2;bash do.sh kill DBU'
+
+su - a_mix_$2 -c 'cd /home/a_mix_$2;bash do.sh allb'
+end
+
+#if [ -f /opt/${1}_${2}_${3}_${4}.sh ];then
+#	bash /opt/${1}_${2}_${3}_${4}.sh
+#else
+#	exit 1
+#fi
