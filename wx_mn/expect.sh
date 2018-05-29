84ec7520be7a49012cb8d0cb319603273a8345f8
Papaya <307798536@qq.com>
2018-05-29 16:43:29 +0800

    Update expect.sh

--- wx_mn/expect.sh
+++ wx_mn/expect.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 ###  used for the machine of new add
+### 
+sed -i '/$1/d' /root/.ssh/known_hosts
+###
+
 expect << EOF &> /dev/null
 spawn scp /root/.ssh/id_rsa.pub $1:/root/.ssh/authorized_keys
 expect "no)?"
