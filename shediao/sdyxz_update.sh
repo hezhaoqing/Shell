8504fa9364b8e01cc3b7a26ba3872ac7fee6c072
Papaya <307798536@qq.com>
2018-07-25 10:05:14 +0800

    Update sdyxz_update.sh

--- shediao/sdyxz_update.sh
+++ shediao/sdyxz_update.sh
@@ -20,25 +20,34 @@ case $2 in
 for i in `diff iOS/ test/iOS/ |grep "Only in test/iOS/"|grep iOS_ |awk '{print $NF}'`
 do
 	cp test/iOS/$i iOS/
+	ls -l iOS/$i
 done
 
-\cp test/iOS/patchlist.xml iOS/                               ### 不用加-f,也可以强制覆盖。
+\cp test/iOS/patchlist.xml iOS/                                ### 不用加-f,也可以强制覆盖。
+ls -l iOS/patchlist.xml
 
 sleep 1
 
 for j in `diff Android/ test/Android/ |grep "Only in test/Android/"|grep Android_ |awk '{print $NF}'`
 do
         cp test/Android/$j Android/
+	ls -l Android/$j
 done
 
 \cp test/Android/patchlist.xml Android/
+ls -l Android/patchlist.xml
 ;;
 	s)
 	\cp test/Android/serverlist.xml Android/
 	\cp test/iOS/serverlist.xml iOS/
+	ls -l Android/serverlist.xml
+	ls -l iOS/serverlist.xml
 	;;
 
 	*)
 	echo "Invalid argument. \$2: Only p or s."
+	exit 1
 	;;
 esac
+
+printf "Update completed, please flush CDN.\n"
