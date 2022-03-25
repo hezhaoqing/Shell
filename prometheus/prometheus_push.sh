38adcfaa6180696d29191ea28cddbab41dc80ef8
Papaya <307798536@qq.com>
2022-03-25 10:56:52 +0800

    Update prometheus_push.sh

--- prometheus/prometheus_push.sh
+++ prometheus/prometheus_push.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-
-ps -ef |grep -v grep |grep 'bash prometheus_push.sh' &&  exit 0
-
-
-cd /opt/shellscripts  && \
-nohup bash prometheus_push.sh &
-[root@txy-sh2-epl-backend-prod1 shellscripts]# cat prometheus_push.sh
-#!/bin/bash
 #####
 
 myip=`ip a |awk '/global eth0/{print $2}'|awk -F "/" '{print $1}'`
